b480d3c7ac839e20aff8307109e1ee257f6b79cd
Hector Reyes Aleman <hector@eclecticiq.com>
2018-06-01 17:11:43 +0200

    add output root_resource_id

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "resource_id" {
 output "resource_path" {
   value = "${aws_api_gateway_resource.api-resource.path}"
 }
+
+output "root_resource_id" {
+  value = "${aws_api_gateway_rest_api.api.root_resource_id}"
+}
